9b18f5cc1f541d9ef1213d406ed8729c71ce589a
Nguyễn Đức Thái <thaihawai@gmail.com>
2022-01-05 22:57:00 +0700

    2 color mode

--- back-end/app.py
+++ back-end/app.py
@@ -26,20 +26,20 @@ from utils import colorize_test, display_test, extract_roi, colorize_page, displ
 file_name = 'pictures/sample_page.png'
 model_name_1 = 'model_unet_resnet_rgb_color'
 model_name_2 = 'unet_rgb'
-model_name = model_name_1
 resnet = tf.keras.applications.resnet50.ResNet50(include_top=True, weights='imagenet', classes=1000)
-model = tf.keras.models.load_model(model_name)
+model_rgb = tf.keras.models.load_model(model_name_1)
+model_color_rgb = tf.keras.models.load_model(model_name_2)
 roi_ls = extract_roi(file_name, threshold_roi=1/16)
 
-def predict_mode_1(color_mode=True, color_space='rgb', filename=None):
-    output, y_img = colorize_page(model, roi_ls, resnet=resnet, color_space=color_space, color_mode=color_mode)
+def predict_mode_1(color_mode=False, color_space='rgb', filename=None):
+    output, y_img = colorize_page(model_rgb, roi_ls, resnet=resnet, color_space=color_space, color_mode=color_mode)
     self_color = display_page(filename, output, y_img, color_space='rgb')
     return self_color
 
 
 # def predict_mode_2(color_mode=True, color_space='rgb', src_img='pictures/sample_3.jpg'):
 def predict_mode_2(color_mode=True, color_space='rgb', src_img=None, filename=None):
-    output, y_img = colorize_page(model, roi_ls, resnet=resnet, color_space=color_space, color_mode=color_mode,
+    output, y_img = colorize_page(model_color_rgb, roi_ls, resnet=resnet, color_space=color_space, color_mode=color_mode,
                                   src_img=src_img)
     img_color = display_page(filename, output, y_img, color_space='rgb')
     return img_color
@@ -47,7 +47,7 @@ def predict_mode_2(color_mode=True, color_space='rgb', src_img=None, filename=No
 
 # def predict_mode_3(color_mode=True, color_space='rgb', color=[(255, 0, 0, 1), (0, 255, 0, 1), (55, 200, 255, 1)]):
 def predict_mode_3(color_mode=True, color_space='rgb', color=None, filename=None):
-    output, y_img = colorize_page(model, roi_ls, resnet=resnet, color_space=color_space, color_mode=color_mode, src_color=color)
+    output, y_img = colorize_page(model_color_rgb, roi_ls, resnet=resnet, color_space=color_space, color_mode=color_mode, src_color=color)
     palette_color = display_page(filename, output, y_img, color_space='rgb')
     return palette_color
 
