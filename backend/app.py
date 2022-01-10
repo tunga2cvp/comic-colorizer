0d2be0f908df8706c9a78d9bbdb234826f37fd04
longtrinh1749 <trinhlong1749@gmail.com>
2022-01-11 00:39:21 +0700

    them try catch cho khoi bi giet

--- backend/app.py
+++ backend/app.py
@@ -34,34 +34,39 @@ class Image(Resource):
 @api.route('/api/v1/predict/<int:mode>', methods=['POST'])
 class ColorizedImage(Resource):
     def post(self, mode):
-        # args = colorize_param.parse_args()
-        files = request.files
-        data = request.form
+        try:
+            # args = colorize_param.parse_args()
+            files = request.files
+            data = request.form
 
-        source_image = files.get('source_image')
-        source_image.save(f'{config.SOURCE_IMAGES_PATH}/{source_image.filename}')
+            source_image = files.get('source_image')
+            source_image.save(f'{config.SOURCE_IMAGES_PATH}/{source_image.filename}')
 
-        if mode == 1:
-            predict(mode=1, filename=source_image.filename, threshold=float(data.get('threshold', '0')))
-            return send_file(f'{config.RESULT_IMAGES_PATH}/{source_image.filename}', mimetype=source_image.mimetype)
-        elif mode == 2:
-            supplement_image = files.get('supplement_image')
-            supplement_image.save(f'{config.SUPPLEMENT_IMAGES_PATH}/{supplement_image.filename}')
-            predict(mode=2, filename=source_image.filename, src_img=supplement_image.filename,
-                    threshold=float(data.get('threshold', '0')))
-            return send_file(f'{config.RESULT_IMAGES_PATH}/{source_image.filename}', mimetype=source_image.mimetype)
-        elif mode == 3:
-            color = data.get('color').split(',')
-            for c in color:
-                c = int(c)
-            color = [(color[0], color[1], color[2], color[3]),
-                     (color[4], color[5], color[6], color[7]),
-                     (color[8], color[9], color[10], color[11])]
-            predict(mode=3, filename=source_image.filename, color=color, threshold=float(data.get('threshold', '0')))
-            return send_file(f'{config.RESULT_IMAGES_PATH}/{source_image.filename}', mimetype=source_image.mimetype)
-        else:
+            if mode == 1:
+                predict(mode=1, filename=source_image.filename, threshold=float(data.get('threshold', '0')))
+                return send_file(f'{config.RESULT_IMAGES_PATH}/{source_image.filename}', mimetype=source_image.mimetype)
+            elif mode == 2:
+                supplement_image = files.get('supplement_image')
+                supplement_image.save(f'{config.SUPPLEMENT_IMAGES_PATH}/{supplement_image.filename}')
+                predict(mode=2, filename=source_image.filename, src_img=supplement_image.filename,
+                        threshold=float(data.get('threshold', '0')))
+                return send_file(f'{config.RESULT_IMAGES_PATH}/{source_image.filename}', mimetype=source_image.mimetype)
+            elif mode == 3:
+                color = data.get('color').split(',')
+                for c in color:
+                    c = int(c)
+                color = [(color[0], color[1], color[2], color[3]),
+                         (color[4], color[5], color[6], color[7]),
+                         (color[8], color[9], color[10], color[11])]
+                predict(mode=3, filename=source_image.filename, color=color, threshold=float(data.get('threshold', '0')))
+                return send_file(f'{config.RESULT_IMAGES_PATH}/{source_image.filename}', mimetype=source_image.mimetype)
+            else:
+                return {
+                    'msg': "Sai mode tô màu ảnh."
+                }
+        except:
             return {
-                'msg': "Sai mode tô màu ảnh."
+                'msg': "Server bị lỗi."
             }
 
 
